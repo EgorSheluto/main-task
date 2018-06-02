8206815856ba2eb9cbb917c7a63755161131bd2d
EgorSheluto <39813321+EgorSheluto@users.noreply.github.com>
2018-06-02 10:37:41 +0300

    Add classes for work with DataBase

---
+++ Intervale/INTERVALE/Constants.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTERVALE
+{
+    class Constants
+    {
+        public const string SQL_CREATE_TABLE_EMPLOYEE = ("CREATE TABLE [IF NOT EXISTS] Employee (ID INT NOT NULL,"+
+            "Surname VARCHAR(50) NOT NULL,"+
+            "Name VARCHAR(20) NOT NULL," +
+            "Patronymic VARCHAR(50) NOT NULL," +
+            "BornDate DATE NOT NULL," +
+            "EmplDate DATE NOT NULL," +
+            "PRIMARY KEY (ID));");
+
+        public const string SQL_CREATE_TABLE_MANAGER = ("CREATE TABLE [IF NOT EXISTS] Manager (ID INT NOT NULL," +
+            "Surname VARCHAR(50) NOT NULL," +
+            "Name VARCHAR(20) NOT NULL," +
+            "Patronymic VARCHAR(50) NOT NULL," +
+            "BornDate DATE NOT NULL," +
+            "EmplDate DATE NOT NULL," +
+            "PRIMARY KEY (ID));");
+
+        public const string SQL_CREATE_TABLE_OTHER = ("CREATE TABLE [IF NOT EXISTS] Other (ID INT NOT NULL," +
+            "Surname VARCHAR(50) NOT NULL," +
+            "Name VARCHAR(20) NOT NULL," +
+            "Patronymic VARCHAR(50) NOT NULL," +
+            "BornDate DATE NOT NULL," +
+            "EmplDate DATE NOT NULL," +
+            "Description VARCHAR(50)," +
+            "PRIMARY KEY (ID));");
+
+        public const string SQL_CREATE_TABLE_MANAGER_LIST_OF_STAFF = ("CREATE TABLE [IF NOT EXISTS] Manager_list_of_staff " + 
+            "(ID INT NOT NULL AUTO_INCREMENT," +
+            "ID_Manager INT NOT NULL," +
+            "ID_Staff INT NOT NULL," +
+            "PRIMARY KEY (ID));");
+
+        public const string SQL_DROP_ALL_TABLES = ("DROP TABLE test_db.Employee, test_db.Manager, test_db.Other, test_db.Manager_list_of_staff;");
+
+        public const string SQL_INSERT_EMPLOYEE = "INSERT INTO Employee VALUES ({0}, '{1}', '{2}', '{3}', '{4}', '{5}')";
+
+        public const string SQL_INSERT_MANAGER = "INSERT INTO Manager VALUES ({0}, '{1}', '{2}', '{3}', '{4}', '{5}')";
+
+        public const string SQL_INSERT_OTHER = "INSERT INTO Other VALUES ({0}, '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')";
+
+        public const string SQL_INSERT_MANAGER_LIST_OF_STAFF = "INSERT INTO Manager_list_of_staff (ID_Manager, ID_Staff) VALUES ({0}, {1})";
+    }
+}
---
+++ Intervale/INTERVALE/DBConnect.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace INTERVALE
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect()
+        {
+            Initialize();
+            CreateTables();
+        }
+
+        private void Initialize()
+        { 
+            server = "localhost";
+            database = "test_db";
+            uid = "root";
+            password = "";
+            string connectionString = "";
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Can't connect to server. Contact administrator");
+                        break;
+                    case 1:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        private void CreateTables()
+        { 
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand command = new MySqlCommand(Constants.SQL_CREATE_TABLE_EMPLOYEE + "\n" + Constants.SQL_CREATE_TABLE_MANAGER + "\n" + Constants.SQL_CREATE_TABLE_OTHER + "\n" + Constants.SQL_CREATE_TABLE_MANAGER_LIST_OF_STAFF, connection);
+
+                command.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void InsertEmployee(string obj)
+        {
+            if (this.OpenConnection() == true)
+            {
+                string[] datas = obj.Split('\t');
+
+                MySqlCommand command = new MySqlCommand(String.Format(@Constants.SQL_INSERT_EMPLOYEE, datas[0], datas[1], datas[2], datas[3], datas[4], datas[5]), connection);
+
+                command.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void InsertManager(string obj)
+        {
+            if (this.OpenConnection() == true)
+            {
+                string[] datas = obj.Split('\t');
+
+                MySqlCommand command = new MySqlCommand(String.Format(@Constants.SQL_INSERT_MANAGER, datas[0], datas[1], datas[2], datas[3], datas[4], datas[5]), connection);
+
+                command.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void InsertOther(string obj)
+        {
+            if (this.OpenConnection() == true)
+            {
+                string[] datas = obj.Split('\t');
+
+                MySqlCommand command = new MySqlCommand(String.Format(@Constants.SQL_INSERT_MANAGER, datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6]), connection);
+
+                command.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        public void InsertManager_list_of_staff(string obj)
+        {
+            if (this.OpenConnection() == true)
+            {
+                string idManager = obj.Split('\t')[0];
+                string[] datas = obj.Split('\t')[6].Split(',');
+
+                for (int i = 0; i < datas.Length; i++)
+                {
+                    MySqlCommand command = new MySqlCommand(String.Format(@Constants.SQL_INSERT_MANAGER_LIST_OF_STAFF, Convert.ToInt32(idManager), Convert.ToInt32(datas[i])), connection);
+
+                    command.ExecuteNonQuery();
+                }
+
+                this.CloseConnection();
+            }
+        }
+
+        public void DropTables()
+        { 
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand command = new MySqlCommand(Constants.SQL_DROP_ALL_TABLES, connection);
+
+                command.ExecuteNonQuery();
+
+                this.CloseConnection();    
+            }
+        }
+    }
+}
